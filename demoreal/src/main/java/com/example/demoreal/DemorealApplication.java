95b32a94e1fa5f8285fa901c220d7224dd537ceb
xiewenxiu <1840565983@qq.com>
2018-12-29 16:23:14 +0800

    dd

--- demoreal/src/main/java/com/example/demoreal/DemorealApplication.java
+++ demoreal/src/main/java/com/example/demoreal/DemorealApplication.java
@@ -2,13 +2,13 @@ package com.example.demoreal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class DemorealApplication {
 
+public class DemorealApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemorealApplication.class, args);
     }
-
 }
 
