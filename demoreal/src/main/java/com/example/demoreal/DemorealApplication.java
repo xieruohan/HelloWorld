4a613ad11561829418090a303da8742e644545ef
xiewenxiu <1840565983@qq.com>
2018-12-29 14:51:39 +0800

    第一次提交

---
+++ demoreal/src/main/java/com/example/demoreal/DemorealApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemorealApplication.class, args);
+    }
+
+}
+
